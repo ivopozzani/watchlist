40a1369b4d1ba5731e234ff6a6e65025278ae46b
ivopozzani <ivo.pozzani@gmail.com>
2022-04-07 13:42:11 -0300

    feat: added alert message

--- app/sidekiq/update_asset_quote_job.rb
+++ app/sidekiq/update_asset_quote_job.rb
@@ -2,7 +2,13 @@
 
 class UpdateAssetQuoteJob
   include Sidekiq::Job
-  sidekiq_options retry: 3
+  sidekiq_options retry: 5
+
+  sidekiq_retries_exhausted do |job, exception|
+    asset = Asset.find_by!(symbol: job['args'].first.upcase, currency: 'BRL')
+    quote = Quote.create(price: 0, current: false, asset_id: asset.id)
+    Alert.create!(message: "Something went wrong, error: #{exception}", quote_id: quote.id)
+  end
 
   def perform(asset_ticker, currency = 'BRL')
     asset = Asset.find_or_create_by(symbol: asset_ticker.upcase, currency: currency)
